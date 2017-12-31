dbdee43cb1ae5662a6741b21c5c79536d4d7078a
gergopc <gergoesjanka@gmail.com>
2017-12-31 15:31:16 +0200

    asd

--- main.cpp
+++ main.cpp
@@ -1,9 +1,25 @@
 #include <iostream>
+#include <fstream>
+#include <string.h>
+
+#define START 0x10
+#define STOP 0xFF
+#define STARTUSERNAME 0x11
+#define STOPUSERNAME 0x12
+#define STARTMESSAGE 0x13
+#define STOPMESSAGE 0x14
+
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    return 0;
+ifstream fin("in.msg");
+string a;
+fin>>a;
+while(fin.eof()==false){
+    cout<<a;
+    fin>>a;
+
+}
 }
