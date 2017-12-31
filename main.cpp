0e62b6e026e65e72c4d7f117b34eaa25623ac389
gergopc <gergoesjanka@gmail.com>
2017-12-31 16:11:31 +0200

    alakul

--- main.cpp
+++ main.cpp
@@ -18,8 +18,33 @@ ifstream fin("in.msg");
 string a;
 fin>>a;
 while(fin.eof()==false){
+    bool read;
+    bool un_mode;
+    bool msg_mode;
     cout<<a;
-    fin>>a;
+    int p=0;
+    int sector=999;
+    string sec;
+    while(sector>0){
+    sec.clear();
+    sec=a.substr(p, 3);
+    p+=3;
+    sector=atoi(sec.c_str());
+    if(sector==START) read=true;
+    if(sector==STOP){
+            read=false;
+            sector=0;
+    }
+    if(sector==STARTUSERNAME) un_mode=true;
+    if(sector==STOPUSERNAME) un_mode=false;
+    if(sector==STARTMESSAGE) msg_mode=true;
+    if(sector==STOPMESSAGE) msg_mode=false;
+    if(read==true){
 
+
+    }
+    }
+cout<<username<<": "<<message;
+fin>>a;
 }
 }
